8006bbc6f24a2d73279c4ff1ceba0729ed2fb535
Max-M <maxnmagura@gmail.com>
2019-09-29 21:29:30 -0500

    added movie OMDB functionality, minus the rotten tomatoes rating

--- liri.js
+++ liri.js
@@ -6,14 +6,12 @@ var keys = require("./keys.js");
 
 // var spotify = new Spotify(keys.spotify);
 
-console.log("Hey, this is working.")
-
 if (process.argv[2] === "concert-this") {
-  var artist = process.argv.slice(3).join(" ")
+  var artist = process.argv.slice(3).join("+")
 
   axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
   function(response) {
-    console.log(artist + " is playing at ------> " + '\n');
+    console.log(process.argv.slice(3).join(" ") + " is playing at ------> " + '\n');
     // console.log(response.data)
     for (i = 0; i < response.data.length; i ++) {
       console.log(
@@ -26,4 +24,41 @@ if (process.argv[2] === "concert-this") {
   })
 }
 
-// node liri.js concert-this ariana grande 
+if (process.argv[2] === "movie-this") {
+  var movie = process.argv.slice(3).join("+")
+
+  if (process.argv[3] === undefined) {
+    axios.get("http://www.omdbapi.com/?t=mr+nobody+&y=&plot=short&apikey=trilogy").then(
+    function(response) {
+    console.log(
+      "Title: " + response.data.Title + '\n' +
+      "Release Year: " + response.data.Released + '\n' +
+      "IMDB Rating: " + response.data.imdbRating + '\n'
+      )
+      //"Rotten Tomatoes Rating: " + response.data.Ratings[1].Value  + '\n' +
+      console.log(
+      "Country Movie Premiered: " + response.data.Country + '\n' +
+      "Language: " + response.data.Language + '\n' +
+      "Plot: " + response.data.Plot + '\n' +
+      "Actors: " + response.data.Actors
+      )
+    })
+  }
+
+  else {axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log(
+      "Title: " + response.data.Title + '\n' +
+      "Release Year: " + response.data.Released + '\n' +
+      "IMDB Rating: " + response.data.imdbRating + '\n'
+      )
+      //"Rotten Tomatoes Rating: " + response.data.Ratings[1].Value  + '\n' +
+      console.log(
+      "Country Movie Premiered: " + response.data.Country + '\n' +
+      "Language: " + response.data.Language + '\n' +
+      "Plot: " + response.data.Plot + '\n' +
+      "Actors: " + response.data.Actors
+      )
+  })
+  }
+}
